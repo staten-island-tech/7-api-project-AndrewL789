89b416acfc41802beeee8d702cb4299f9f1249e3
AndrewL789 <Andrewl434@nycstudents.net>
2025-12-03 09:26:12 -0500

    api keuy where

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
 import requests
+import random
 # replace all of the words in a text wit random synonyms
 def word(word):
-        bore = requests.get(f'https://wordsapiv1.p.mashape.com/words/{word.lower()}')
-        data = bore.json()
+        bore = requests.get(f'https://wordsapiv1.p.rapidapi.com/words/{word.lower()}/synonyms')
+        if bore.status_code != 200:
+          print("Error fetching data")
+          return None
+        data = bore.json
+        word = random.data
         
     
-print(word('fart'))
+print(word('boat'))
