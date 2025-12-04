6485737c9cb4b49e2df078a6f7532f4737306195
AndrewL789 <Andrewl434@nycstudents.net>
2025-12-04 09:26:03 -0500

    anime wth

--- api.py
+++ api.py
@@ -2,12 +2,10 @@ import requests
 import random
 # replace all of the words in a text wit random synonyms
 def word(word):
-        bore = requests.get(f'https://wordsapiv1.p.rapidapi.com/words/{word.lower()}/synonyms')
+        bore = requests.get(f'https://api.jikan.moe/v4/')
         if bore.status_code != 200:
           print("Error fetching data")
           return None
-        data = bore.json
-        word = random.data
-        
-    
+        data = bore.json 
+        return type(data)   
 print(word('boat'))
