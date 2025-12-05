3cf6b7f618ef60d679286acb25e1a4b792d48c65
AndrewL789 <Andrewl434@nycstudents.net>
2025-12-05 03:26:08 +0000

    t kinter time

--- api.py
+++ api.py
@@ -1,11 +1,28 @@
 import requests
 import random
-# replace all of the words in a text wit random synonyms
-def word(word):
-        bore = requests.get(f'https://api.jikan.moe/v4/')
-        if bore.status_code != 200:
+#anime wordle 
+#https://api.jikan.moe/v4/ <- the url base 
+def randoman():
+     popular = requests.get("https://api.jikan.moe/v4/top/anime")
+     if popular.status_code != 200:
           print("Error fetching data")
           return None
-        data = bore.json 
-        return type(data)   
-print(word('boat'))
+     data = popular.json()
+     anime = random.choice(data['data'])
+     name = [anime['title'], anime['title_english']] + anime['title_synonyms']
+     names = [n.lower() for n in name if n is not None]
+     hints = {'type': anime['type'], 'genre' :[g['name'] for g in anime['genres']], 'source': anime['source'], 'episodes' : anime['episodes'], 'rating' : anime['rating'], 'score' : anime['score'], 'popularity' : anime['popularity']}
+     return names, hints
+def game():
+     names, hints = randoman()
+     print(hints)
+     print(names) # take this out at end
+     correct = False
+     inputamt = 0
+     while correct == False and inputamt < 5:
+          guess = input('guess? :').strip().lower()
+          inputamt += 1
+          if guess in names:
+               correct = True
+               print(f'correct! got it in {inputamt}')
+game()
