589ccf11012d05944b3944d401f3e7f06ceacb10
AndrewL789 <Andrewl434@nycstudents.net>
2025-12-05 09:25:36 -0500

    grah grah boom

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 import requests
 import random
+import tkinter as tk
 #anime wordle 
 #https://api.jikan.moe/v4/ <- the url base 
+def anicheck(x):
+     thething = requests.get("https://api.jikan.moe/v4/anime?q={x}")
+     gah = thething.json()
+     compare = gah['data']
+     return compare
 def randoman():
      popular = requests.get("https://api.jikan.moe/v4/top/anime")
      if popular.status_code != 200:
@@ -9,13 +15,16 @@ def randoman():
           return None
      data = popular.json()
      anime = random.choice(data['data'])
+     return anime
+def analyze(anime):
      name = [anime['title'], anime['title_english']] + anime['title_synonyms']
      names = [n.lower() for n in name if n is not None]
-     hints = {'type': anime['type'], 'genre' :[g['name'] for g in anime['genres']], 'source': anime['source'], 'episodes' : anime['episodes'], 'rating' : anime['rating'], 'score' : anime['score'], 'popularity' : anime['popularity']}
-     return names, hints
+     qualities = {'type': anime['type'], 'genre' :[g['name'] for g in anime['genres']], 'source': anime['source'], 'episodes' : anime['episodes'], 'rating' : anime['rating'], 'score' : anime['score'], 'popularity' : anime['popularity']}
+     return names, qualities
 def game():
-     names, hints = randoman()
-     print(hints)
+     x = randoman()
+     names, qualities = analyze(x)
+     print(qualities)
      print(names) # take this out at end
      correct = False
      inputamt = 0
@@ -25,4 +34,18 @@ def game():
           if guess in names:
                correct = True
                print(f'correct! got it in {inputamt}')
-game()
+          if inputamt == 5 and correct == False:
+               print(f'You lose, it was {names}')
+#game()
+window = tk.Tk()
+window.title = ('Wordle')
+window.geometry("960x540")
+prompt = tk.Label(window, text="Guess the anime:",
+font=("Arial", 14))
+prompt.pack(pady=10)
+entry = tk.Entry(window, font=("Arial", 14), width=30)
+entry.pack(pady=5)
+result_label = tk.Label(window, text="", font=("Arial", 14, "bold"),
+fg="blue")
+result_label.pack(pady=15)
+window.mainloop()
