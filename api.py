920895ddbeb6ac774da471c8d8e24ea1321c0d47
AndrewL789 <Andrewl434@nycstudents.net>
2025-12-15 09:19:41 -0500

    make it look better

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import requests
 import random
 import tkinter as tk
 from tkinter import ttk 
+import threading
 #anime wordle 
 #https://api.jikan.moe/v4/ <- the url base
 def anicheck(x):
@@ -22,49 +23,8 @@ def analyze(anime):
      name = [anime['title'], anime['title_english']] + anime['title_synonyms']
      names = [n.lower() for n in name if n is not None]
      qualities = {'type': anime['type'], 'genre' :[g['name'] for g in anime['genres']], 'source': anime['source'], 'episodes' : anime['episodes'], 'rating' : anime['rating'], 'score' : anime['score'], 'popularity' : anime['popularity']}
-     return names, qualities
-def gamescreen():
-
-     x = randoman()
-     names, qualitie = analyze(x)
-     window = tk.Tk()
-     window.title = ('Wordle')
-     window.geometry("960x540")
-     window.configure(bg="#2C2F33")
-     def update(event=None):
-          text = drop.get()
-          compare, _ = anicheck(text)
-          drop['values'] = compare
-     def on_enter(event=None):
-          if drop.get() in drop['values']:
-               value = drop.get()
-               _, senor = anicheck(value)
-               _ , qualities = analyze(senor)
-               text = f"Guess: {value}\n" \
-               f"Type: {qualities['type']}\n" \
-               f"Genres: {', '.join(qualities['genre'])}\n" \
-               f"Source: {qualities['source']}\n" \
-               f"Episodes: {qualities['episodes']}\n" \
-               f"Rating: {qualities['rating']}\n" \
-               f"Score: {qualities['score']}\n" \
-               f"Popularity: {qualities['popularity']}"
-               te(text)
-               drop.delete(0, tk.END)
-     def te(x):
-          new_text= tk.Text(window, height = 10, width = 180,bg = "#23272A", font=("Arial", 10))
-          new_text.insert("end", x)
-          new_text.config(state="disabled")
-          new_text.pack(pady=9)
-          gah.append(new_text)
-     drop = ttk.Combobox(window)
-     drop['values'] = []
-     drop.set("")
-     drop.pack(pady = 10)
-     drop.bind("<KeyRelease>", update)
-     drop.bind("<Return>", on_enter)
-     gah = [] 
-
-     window.mainloop()
+     id = anime['mal_id']
+     return names, qualities, id
 class game:
      def __init__(self):
           self.window = tk.Tk()
@@ -72,15 +32,13 @@ class game:
           self.window.geometry("960x540")
           self.window.configure(bg="#2C2F33")
           self.x = randoman()
-          self.names, self.qualitie = analyze(self.x)
+          self.names, self.qualitie, self.id = analyze(self.x)
           self.guess_count = 0
           self.gah=[]
           self.drop = ttk.Combobox(self.window)
           self.drop['values'] = []
           self.drop.set("")
           self.drop.pack(pady=10)
-          self.label = tk.Label(self.window, text = self.names,bg="#2C2F33",fg="#FFFFFF")
-          self.label.pack(side = 'left')
           self.drop.bind("<KeyRelease>", self.update)
           self.drop.bind("<Return>", self.on_enter)
           self.win = False
@@ -99,7 +57,7 @@ class game:
           if self.drop.get() in self.drop['values'] and self.guess_count < 5 and self.win == False and self.lose == False:
                value = self.drop.get()
                _, senor = anicheck(value)
-               _ , qualities = analyze(senor)
+               _ , qualities, id = analyze(senor)
                output = self.xi(qualities)
                self.guess_count += 1
                text = (
@@ -115,17 +73,18 @@ class game:
                )
                self.te(text)
                self.drop.delete(0, tk.END)
-          for t in self.names:
-               if value == t:
-                         self.win = True
+               if id == self.id:
+                    self.win = True
           if self.guess_count == 5 and self.win == False:
                self.lose = True
           if self.win == True:
-               self.te('You win!')
+               self.te(f'You win! in {self.guess_count} attempts!')
+               self.drop.destroy()
+               return
           if self.lose == True:
-               self.te(f'You lose, it was ')
-          
-               
+               self.te(f'You lose, it was {self.x['title_english'] or self.x['title']}')
+               self.drop.destroy()
+               return           
      def run(self):
           self.window.mainloop()
      def xi(self, x):
